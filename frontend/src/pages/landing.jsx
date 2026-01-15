9598b8749625d3f98eec2570411330af1a840db2
Manish-Baviskar <manish.218007@gmail.com>
2026-01-16 00:15:12 +0530

    Created a info modal and footer

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import styles from "../styles/landing.module.css";
 import AboutModal from "./AboutModal";
